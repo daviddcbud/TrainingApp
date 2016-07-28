77a1844620c4f734e8a03868b28c71196b37cae3
David Freeman <david@dd.com>
2016-07-28 11:58:00 -0500

    changes

---
+++ Projects/Starter/Starter/scripts/myscript.js
@@ -0,0 +1,12 @@
+﻿document.getElementById("submit").onclick = function () {
+
+    var firstName = document.getElementById('firstName').value;
+
+    var lastName = document.getElementById('lastName').value;
+
+    var fullName = firstName + '  ' + lastName;
+
+    document.getElementById('fullName').innerText = fullName;
+
+};
+
